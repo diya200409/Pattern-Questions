ba3ce66f5105b9c79e0df08236747ba59b340bc5
diya200409 <diyaumale9@gmail.com>
2026-07-06 23:56:07 +0530

    day3

--- square_pattern.py
+++ square_pattern.py
@@ -4,3 +4,20 @@ def square(n):
             print("*", end="")
         print()
 square(4)
+
+
+'''Problem Statement
+
+Given an integer N, return a square pattern of size N × N made up of the character '*'.
+
+Input:
+
+N = 4
+
+Output:
+
+****
+****
+****
+****
+'''
